f3d72158b3bc8347a201f37292715c999bf48013
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-09-07 00:26:12 +0200

    Feat : add Router and refacto

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,24 @@
-import express, { Response, Request} from 'express';
-import { connectToDb, getCollection } from './utils';
-import { config } from "dotenv"
+import express from 'express';
+import { connectToDb } from './utils';
+import { config } from 'dotenv';
 config();
+
+import useRouter from './routes/index';
 
 const app = express();
 const port = process.env.PORT;
-
-app.get('/', (req, res ) => {
-  res.send('Hello World!');
-});
 
 const startServer = async () => {
   try {
     connectToDb();
     app.listen(port, () => {
-      return console.log(`Express is listening at http://localhost:${port}`);
+      console.log(`Express is listening at http://localhost:${port}`);
     });
+
+    app.use(useRouter);
+
   } catch (error) {
-    console.warn(error)
+    console.warn(error);
   }
-}
+};
 startServer();
-const collection = getCollection("test");
