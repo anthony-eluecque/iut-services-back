b53f7ed5fccfd321eb0505aa6b81c3623e14b8c9
Houssam CUMZAIN <doriancoriace@gmail.com>
2023-09-14 00:45:11 +0200

    feat(models): create Teacher model

--- src/entities/teacher.entity.ts
+++ src/entities/teacher.entity.ts
@@ -3,5 +3,15 @@ import Model from "./model.entity"
 
 @Entity('teachers')
 export class Teacher extends Model {
+    @Column()
+    givenId: string
 
+    @Column()
+    lastName: string
+
+    @Column()
+    firstName: string
+
+    @Column()
+    roleId: string
 }
