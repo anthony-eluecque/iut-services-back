978f0db5c5da42757754047b7742e89ee92fe1db
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-09-27 12:49:56 +0200

    Imp: clean dataSource + add config for npm scripts

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,12 +1,8 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "../entities";
-import { Lesson } from "../entities/lesson.entity";
-import { Teacher } from "../entities/teacher.entity";
-import { Item } from "../entities/item.entity";
-import { Role } from "../entities/role.entity";
-import { Service } from "../entities/service.entity";
+import { config } from "dotenv";
 
+config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -17,7 +13,8 @@ export const AppDataSource = new DataSource({
     database: String(process.env.POSTGRES_DB),
     synchronize: true,
     logging: false,
-    entities: [Lesson, Item, User, Teacher, Role, Service],
-    migrations: [],
+    entities: ["./src/entities/*.ts"],
+    migrations: ["./src/migrations/*.ts"],
     subscribers: [],
+    
 });
