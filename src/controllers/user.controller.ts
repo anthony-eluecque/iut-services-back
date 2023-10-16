59eb87acc04d76fb6814e7420c15289f2e07e32b
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-16 14:14:51 +0200

    Feat: user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,25 +2,68 @@ import { Request, Response } from 'express';
 import { User } from '../entities';
 import { AppDataSource } from '../config/data-source';
 import Res from '../helpers/res.helper';
+import { hashPassword } from '../services/hash.service';
+const usersRepository = AppDataSource.getRepository(User);
+
+import CryptoJS, { AES } from "crypto-js";
+
+var key = "PasswordText";
 
 export const getUsers = async (req : Request, res : Response) => {
     try {
-        const user = await AppDataSource.manager.find(User);
-        return Res.send(res,200,'Success',user);
+        const users = await usersRepository.find({});
+        return Res.send(res,200,'Success',users);
     } catch (error) {
         return Res.send(res,500,'Internal Server error');
     }
 };
 
+export const getUser = async (req : Request, res: Response) => {
+    try {
+        const user = await usersRepository.findOne({where : {
+            id : req.params.id
+        }})
+        if (!user){
+            return Res.send(res,404,'User Not Found');
+        }
+
+        const decryptedUser = {
+            email : user.email,
+            password : user.password,
+            firstName : AES.decrypt(user.firstName,key).toString(CryptoJS.enc.Utf8),
+            lastName : AES.decrypt(user.lastName,key).toString(CryptoJS.enc.Utf8)
+        }
+
+
+        return Res.send(res,200,'Got one',decryptedUser);
+    } catch (error) {
+        return Res.send(res,500,'Internal Server error',error);
+    }
+}
+
 export const createUser = async (req: Request, res: Response) => {
     try {
-        const user = new User();
-        user.firstName = "firstName";
-        user.lastName = "lastName";
-        user.age = 15;
-        await AppDataSource.manager.save(user);
-        return Res.send(res,200,'',user);
+        const hashPwd = await hashPassword(req.body.password)
+        
+        
+        const userEncrypted = {
+            email : req.body.email,
+            password : hashPwd,
+            firstName : AES.encrypt(req.body.firstName,key).toString(),
+            lastName : AES.encrypt(req.body.lastName,key).toString()
+        }
+
+
+        const userFinded = await usersRepository.findOne({where : { email : userEncrypted.email}})
+        if (userFinded){
+            return Res.send(res,409,'User already exist',userFinded)
+        } 
+        const newUser = new User();
+        await usersRepository.merge(newUser,userEncrypted).save()
+
+        return Res.send(res,200,'User has been created',userEncrypted)
     } catch (error) {
-        return Res.send(res,500,'Internal Server error');
+        console.error(error)
+        return Res.send(res,500,'Internal Server error',error);
     }
 };
