0f7be232cf2c718a6a74ba3e8643fbdd3777e0e6
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-17 23:01:44 +0200

    Imp: Clean code + use Validator

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
-import { User } from '../entities';
+import { User, validateUser } from '../entities';
 import { AppDataSource } from '../config/data-source';
 import Res from '../helpers/res.helper';
 import { hashPassword } from '../services/hash.service';
 import CryptoJS, { AES } from "crypto-js";
 import { generateConnectionToken, verifyPassword } from '../services/auth.service';
 import messages from '../docs/messages.json'
-
+import { validate } from "class-validator";
 
 const { serverError } = messages.defaults
 const usersRepository = AppDataSource.getRepository(User);
@@ -47,30 +47,27 @@ export const getUser = async (req : Request, res: Response) => {
 
 export const createUser = async (req: Request, res: Response) => {
     try {
-        const hashPwd = await hashPassword(req.body.password)
+        const { email, password, firstName, lastName } = req.body;
+        const isValidatedUser = await validateUser(req)
         
+        if (!isValidatedUser) return Res.send(res,400,"User not validated");
         
-        const userEncrypted = {
-            email : req.body.email,
-            password : hashPwd,
-            firstName : AES.encrypt(req.body.firstName,key).toString(),
-            lastName : AES.encrypt(req.body.lastName,key).toString()
-        }
-
-
-        const userFinded = await usersRepository.findOne({where : { email : userEncrypted.email}})
+        const hashPwd = await hashPassword(password)
+        
+        const userFinded = await usersRepository.findOne({where : { email : email}})
         if (userFinded){
             return Res.send(res,409,'User already exist',userFinded)
         } 
-        const newUser = new User();
-        await usersRepository.merge(newUser,userEncrypted).save()
+        const newUser = new User(email,hashPwd,AES.encrypt(firstName,key).toString(),AES.encrypt(lastName,key).toString());
+        await usersRepository.save(newUser)
 
-        return Res.send(res,200,'User has been created',userEncrypted)
+        return Res.send(res,200,'User has been created',newUser)
     } catch (error) {
         console.error(error)
         return Res.send(res,500,'Internal Server error',error);
     }
 };
+
 
 //#region  AUTH 
 
@@ -100,9 +97,8 @@ export const login = async (req : Request , res : Response) => {
 
 export const authenticate = async (req: Request, res: Response) => {
     try {
-        if (!res.locals.user)
-            return Res.send(res, 401, "unAuth");
-
+        if (!res.locals.user) return Res.send(res, 401, "unAuth");
+        
         return Res.send(res, 200, "auth", res.locals.user);
     } catch (error) {
         return Res.send(res, 500, serverError);
