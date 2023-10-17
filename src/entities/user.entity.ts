b9dc1742eb650e62565cf87c253245aa9e3248ea
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-17 23:01:21 +0200

    Feat: Handle Validate Email only

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,10 +1,12 @@
 import { Entity, Column } from "typeorm";
 import Model from "./model.entity";
-
+import { IsEmail, validate } from "class-validator";
+import { Request } from "express-serve-static-core";
 @Entity('users')
 export class User extends Model {
 
-    @Column()
+    @Column({unique : true})
+    @IsEmail()
     email: string;  
 
     @Column()
@@ -16,4 +18,19 @@ export class User extends Model {
     @Column()
     lastName: string;
 
+    constructor(email: string, password: string, firstName: string, lastName: string) {
+        super();
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+
+export const validateUser = async (req : Request) => {
+    const { email, password, firstName, lastName } = req.body;
+    const user = new User(email,password,firstName,lastName);
+    const errors = await validate(user);
+    return errors.length == 0 ? true : false
 }
