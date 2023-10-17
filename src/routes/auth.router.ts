d8f89787672cfb9159840346503c5ca11bdc5a6c
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-17 19:21:24 +0200

    Refacto: remove Auth Controller + associates files

--- src/routes/auth.router.ts
+++
@@ -1,9 +0,0 @@
-import express,{ Router } from "express";
-import { generateTokenAndRefreshToken } from "../controllers/auth.controller";
-
-const router = Router();
-
-router.use(express.urlencoded({ extended: false }));
-router.post('/', generateTokenAndRefreshToken)
-
-export default router;
