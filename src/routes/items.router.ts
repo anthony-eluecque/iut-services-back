f947363039ad0be9e17e8a2f666169343e45aac3
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-18 00:54:02 +0200

    Temp: Test + pkg-lock

--- src/routes/items.router.ts
+++ src/routes/items.router.ts
@@ -2,14 +2,13 @@ import express,{ Router } from "express";
 import { getItems, createItem, getPageItems, deleteItemById, updateItem } from "../controllers";
 
 const router = Router();
-const auth = require('../middlewares/auth');
 
 router.use(express.urlencoded({ extended: false }));
-router.get('/', auth, getItems); 
+router.get('/', getItems); 
 // router.post('/', auth, createItem);
 // router.get('/:page', auth, getPageItems);
 // router.delete('/:id', auth, deleteItemById);
-router.put('/', auth, updateItem);
+router.put('/', updateItem);
 
 
 export default router;
