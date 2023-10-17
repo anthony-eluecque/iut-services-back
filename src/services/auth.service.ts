10d331e5bdc94976598a159df73b5b4b9bbb0ce7
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-17 19:24:19 +0200

    Imp: auth Service reworked to respect Jwt Docs

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,43 +1,30 @@
-import { AppDataSource } from "../config/data-source";
+import { CookieOptions, Response } from "express";
 import { User } from "../entities";
 import jwt from "jsonwebtoken"
-import md5 from "md5"
+import CookieHelper from '../helpers/cookie.helper'
+import Res from "../helpers/res.helper";
+import { compare } from "bcrypt";
 
-const usersRepository = AppDataSource.getRepository(User);
 
-export const verifyUserAndGenerateTokenAndRefresh = async (email, password) => {
-	try {
-		let token = null;
-		// Vérification que l'utilisateur existe bien avec son email et mot de passe
-        const user = await usersRepository.findOne({ where: { email, password: md5(password)}});
+export const generateConnectionToken = async (user: User, res: Response) => {
+    const token = await jwt.sign({id: user.id}, process.env.JWT_SECRET);
+    const options: CookieOptions = {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+        path: '/'
+    };
 
-		if (user != null) {
-			// On génère un token
-		   token = jwt.sign({email}, process.env.TOKEN_SECRET, { expiresIn: '600s' });
-		}
-	
-		return token;
-	} catch (e) {
-		console.log(e);
-		throw  e;
-	}
-}
+    CookieHelper.send(res, {
+        name: 'token',
+        value: token,
+        options
+    });
 
-export const generateRefreshToken = async (email) => {
-	try {
-		let refreshToken = null;
-
-		if (email != null) {
-			// On génère un token
-			refreshToken = jwt.sign({email}, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '99960s' });
-		}
-	
-		return refreshToken;
-	} catch (e) {
-		console.log(e);
-		throw  e;
-	}
+    return Res.send(res, 204, "");
 }
 
 
+export const verifyPassword = async (user : User, password : string ) => {
+	return compare(password,user.password);
+}
 
