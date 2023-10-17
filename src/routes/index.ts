dc434ea121a7be653d0bc717eccb6ba64d2423d9
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-17 19:22:26 +0200

    Imp: Clean code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,26 +1,19 @@
 import express,{ Router } from "express";
-const auth = require('../middlewares/auth');
-
-// import useServiceRouter from "./services.routes";
-// import useTeacherRouter from "./teachers.routes";
 import useUsersRouter from "./user.router";
 import useItemsRouter from './items.router';
 import useRolesRouter from './roles.router';
 import useTeachersRouter from './teachers.router';
 import useLessonsRouter from './lessons.router';
 import useServicesRouter from './services.router';
-import authRouter from "./auth.router";
+
 const router = Router();
 
 router.use(express.urlencoded({ extended: false }));
-// router.use('/services',useServiceRouter);
-// router.use('/teachers',useTeacherRouter);
-router.use('/token', authRouter);
 router.use('/users', useUsersRouter);
-router.use('/items',auth,useItemsRouter);
-router.use('/teachers',auth,useTeachersRouter);
-router.use('/roles',auth,useRolesRouter);
-router.use('/lessons',auth,useLessonsRouter);
-router.use('/services',auth,useServicesRouter);
+router.use('/items',useItemsRouter);
+router.use('/teachers',useTeachersRouter);
+router.use('/roles',useRolesRouter);
+router.use('/lessons',useLessonsRouter);
+router.use('/services',useServicesRouter);
 
 export default router;
