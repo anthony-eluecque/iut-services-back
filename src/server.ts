b066d6ae4a78e4129be99823cdc6ef9c0620a083
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-30 18:41:53 +0100

    Feat: Server class for imp lisibility

---
+++ src/server.ts
@@ -0,0 +1,60 @@
+import express,{ Express } from 'express'
+import { config } from 'dotenv';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import useRouter from './routes'
+import { initDbStore } from './config';
+import swaggerUI from 'swagger-ui-express' 
+import swaggerDocument from './swagger.json'
+config()
+export class Server {
+    private app : Express;
+    private port : any = 3000;
+
+    constructor() {
+        this.app = express();
+        this.app.use(cookieParser());
+        this.app.use(bodyParser.json())
+
+        this.app.use(cors({
+            origin : [
+              process.env.CORS_ORIGIN
+            ],
+            credentials : true
+          }));
+
+        this.port = process.env.NODEJS_PORT || this.port;
+    }
+
+    public async initDb(){
+        try {
+            await initDbStore();
+            console.log("Database initialized successfully");
+          } catch (err) {
+            console.error(`Error initializing database: ${err.message}`);
+            process.exit(1); // Arrêtez le processus en cas d'erreur grave
+        }
+    }
+
+    public startServer = () => {
+        this.onServerListen();
+    }
+    
+    private onServerListen = () => {
+        this.app.listen(this.port, () => {
+            console.log(`Express is listening at http://localhost:${this.port}`);
+            console.log(`For exploring the apis open: http://localhost:${this.port}/api-docs`)
+        });
+    }
+
+    public setSwagger = () => {
+        this.app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument, { "showExplorer": true }));
+    }
+
+    public setRoutes = () => {
+        this.app.use(useRouter)
+    }
+
+
+}
