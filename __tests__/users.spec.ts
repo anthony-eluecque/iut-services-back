d92a5c910cf42adec96199961593ac3393f6280b
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-31 16:01:07 +0100

    Imp: User tests

--- __tests__/users.spec.ts
+++ __tests__/users.spec.ts
@@ -8,20 +8,50 @@ server.setRoutes()
 
 const baseUrl = '/users'
 
+const testUser = {
+    firstName : 'Anthony',
+    lastName : 'ELUECQUE',
+    password: 'test',
+    email: 'anthony76520.ae@gmail.com'
+}
+
+const loginUser = {
+    email: 'anthony76520.ae@gmail.com',
+    password: 'test'
+}
+
 const runFirstTest = () => it('Unauthorized to get all users', async () => {
     const response = await request(server.getApp()).get(baseUrl)
     expect(response.statusCode).toBe(401)
 })
 
 const runSecondTest = () => it('Post one user',async () => {
-    const user = {
-        firstName : 'Anthony',
-        lastName : 'ELUECQUE',
-        password: 'test',
-        email: 'anthony76520.ae@gmail.com'
-    }
-    const response = await request(server.getApp()).post(baseUrl).send(user)
+    const response = await request(server.getApp()).post(baseUrl).send(testUser)
     expect(response.statusCode).toBe(200)
+})
+
+const runThirdTest = () => it('Login as user', async () => {
+    const response = await request(server.getApp()).post(baseUrl).send(testUser)
+    const loginResponse = await request(server.getApp()).post(baseUrl+'/login').send(loginUser)
+    expect(loginResponse.statusCode).toBe(204)
+})
+
+
+const runFourthTest = () => it('Login as user and auth myself', async () => {
+    const response = await request(server.getApp()).post(baseUrl).send(testUser)
+    const loginResponse = await request(server.getApp()).post(baseUrl+'/login').send(loginUser)
+    const cookies = loginResponse.headers['set-cookie']
+    const authResponse = await request(server.getApp()).get(baseUrl+'/auth').set('Cookie',cookies)
+    expect(authResponse.statusCode).toBe(200)
+})
+
+
+const runFifthTest = () => it('Login and logout myself', async () => {
+    const response = await request(server.getApp()).post(baseUrl).send(testUser)
+    const loginResponse = await request(server.getApp()).post(baseUrl+'/login').send(loginUser)
+    const cookies = loginResponse.headers['set-cookie']
+    const logoutResponse = await request(server.getApp()).post(baseUrl+'/logout').set('Cookie',cookies)
+    expect(logoutResponse.statusCode).toBe(204)
 })
 
 describe("User routes test",() => {
@@ -31,4 +61,7 @@ describe("User routes test",() => {
     });
     runFirstTest();
     runSecondTest();
+    runThirdTest();
+    runFourthTest();
+    runFifthTest();
 })
--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -6,17 +6,19 @@ import { User } from "../entities";
 import messages from '../docs/messages.json';
 
 const { serverError, unAuth } = messages.defaults
-const usersRepository = AppDataStore.getRepository(User);
 
 export const isAuth = async (req : Request, res : Response, next : NextFunction) => {
     try {
-        const { token } = req.cookies;
+        const usersRepository = AppDataStore.getRepository(User);
 
+        const { token } = req.cookies;
         if (!token) return Res.send(res,401,unAuth);
     
         const { id } = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
+        
         const user = await usersRepository.findOne({where : { id : id }});
         if (!user) return Res.send(res,401, unAuth + ', User not found');
+
 
         res.locals.user = user;
 
