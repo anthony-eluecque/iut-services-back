c9e6e0cd693ab015e278f8355c911fce381d152d
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-11-12 18:21:25 +0100

    Imp: clean code

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,17 +1,16 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from "express"
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import Res from "../helpers/res.helper";
 import { AppDataStore } from "../config";
 import { User } from "../entities";
 import messages from '../docs/messages.json';
 
-const { serverError, unAuth } = messages.defaults;
+const { serverError, unAuth } = messages.defaults
 
 /**
  * Middleware pour vérifier l'authentification de l'utilisateur.
  * Vérifie la présence d'un token dans les cookies, et s'assure que l'utilisateur
- * correspondant à ce token existe dans la base de données. Ajoute l'utilisateur à
- * res.locals.user pour un accès ultérieur.
+ * correspondant à ce token existe dans la base de données. Ajoute l'utilisateur à res.locals.user pour un accès ultérieur.
  * En cas de succès, passe à la prochaine fonction middleware.
  * Si le token est manquant, renvoie une réponse 401 (Non autorisé).
  * Si l'utilisateur n'est pas trouvé, renvoie une réponse 401 (Non autorisé) avec un message indiquant que l'utilisateur n'a pas été trouvé.
@@ -42,4 +41,4 @@ export const isAuth = async (req : Request, res : Response, next : NextFunction)
     } catch (error) {
         return Res.send(res, 500, serverError);
     }
-};
+}
