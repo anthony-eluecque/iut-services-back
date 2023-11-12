7f227f0051948b5c46c4442932fff0d3791fa8f3
Houssam CUMZAIN <doriancoriace@gmail.com>
2023-11-12 23:29:10 +0100

    feat: add LessonType swagger

--- src/entities/lesson_type.entity.ts
+++ src/entities/lesson_type.entity.ts
@@ -5,10 +5,25 @@ import { CustomJoinItemsLessons } from "./joinItemsLessons";
 @Entity('lesson_types')
 export class Lesson_type extends Model {
 
-    @ManyToMany(() => CustomJoinItemsLessons, CustomJoin => CustomJoin.lessonType, { onDelete : 'CASCADE' })
+    @ManyToMany(() => CustomJoinItemsLessons, CustomJoin => CustomJoin.lessonType, { onDelete: 'CASCADE' })
     items: CustomJoinItemsLessons[];
 
     @Column()
     name: string;
-
 }
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Lesson_type:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The name of the lesson type.
+ *       required:
+ *         - name
+ *       example:
+ *         name: CM
+ */
